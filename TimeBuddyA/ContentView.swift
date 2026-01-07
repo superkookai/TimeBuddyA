d0d9d6ec0b884c100748c392955f7035843c4b39
superkookai <weerawut@live.com>
2026-01-07 09:35:55 +0700

    Initial Commit

---
+++ TimeBuddyA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeBuddyA
+//
+//  Created by Weerawut on 7/1/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeBuddyA/TimeBuddyAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBuddyAApp.swift
+//  TimeBuddyA
+//
+//  Created by Weerawut on 7/1/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct TimeBuddyAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
